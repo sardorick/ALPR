6479412ef35f2ce73a7012510f40375557eb5ee7
“sardorick” <sardorzakirov1999@gmail.com>
2023-06-08 13:48:15 +0200

    updated db file

--- static/src/db.py
+++ static/src/db.py
@@ -1,26 +1,56 @@
-from re import L
-from sqlalchemy import create_engine
-import pandas as pd
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-def create_db():
+engine = create_engine('sqlite:///preds.db')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Pred(Base):
+    __tablename__ = 'preds'
+
+    id = Column(Integer, primary_key=True)
+    filename = Column(String)
+    text = Column(String)
+
+    def __init__(self, filename, text):
+        self.filename = filename
+        self.text = text
+
+Base.metadata.create_all(engine)
+
+def add_pred(filename, text):
+    session = Session()
     try:
-        engine = create_engine("sqlite:///preds.db")
-        conn = engine.connect()
-        df = pd.read_csv('./static/upload/preds.csv')
-        df.to_sql("preds", conn, if_exists="replace")
-        conn.close()
-        return 0
-    except:
-        return 1
+        pred = Pred(filename, text)
+        session.add(pred)
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+    finally:
+        session.close()
 
 def get_preds():
+    session = Session()
     try:
-        engine = create_engine("sqlite:///preds.db")
-        conn = engine.connect()
-        df = pd.read_sql_query("SELECT * FROM preds", conn)
-        conn.close()
-        return df.to_dict()
-    except:
+        preds = session.query(Pred).all()
+        return [{'id': pred.id, 'filename': pred.filename, 'text': pred.text} for pred in preds]
+    except Exception as e:
+        print(e)
         return {"404": "No data found"}
+    finally:
+        session.close()
 
-create_db()
+def get_pred_by_filename(filename):
+    session = Session()
+    try:
+        pred = session.query(Pred).filter_by(filename=filename).first()
+        return {'id': pred.id, 'filename': pred.filename, 'text': pred.text}
+    except Exception as e:
+        print(e)
+        return {"404": "No data found"}
+    finally:
+        session.close()
+
+
--- static/src/save_results.py
+++ static/src/save_results.py
@@ -1,17 +1,5 @@
-from flask import request
-import csv
-import os
-import pandas as pd
-from requests import head
 
-BASE_PATH = os.getcwd()
-UPLOAD_PATH = os.path.join(BASE_PATH, 'static/upload/')
+from db import add_pred
 
-def save_results(text, csv_filename='preds.csv', path=UPLOAD_PATH):
-    upload_file_request = request.files['image_name']
-    filename = upload_file_request.filename
-    img_name = f'{filename}.jpg'
-    with open(csv_filename, mode='a', newline='') as f:
-        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([img_name, text])
-        
+def save_results(text, filename):
+    add_pred(filename, text)
